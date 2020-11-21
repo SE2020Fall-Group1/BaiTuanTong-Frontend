732de3031b014ed75c11a948a68f3ed01de28933
Wang Lanshen <1800012829@pku.edu.cn>
2020-11-21 13:52:48 +0800

    WangLanshen Add

--- app/src/main/java/com/example/BaiTuanTong_Frontend/MyAdapter.java
+++ app/src/main/java/com/example/BaiTuanTong_Frontend/MyAdapter.java
@@ -25,6 +25,29 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
         notifyDataSetChanged();
     }
 
+    // 下面是为点击事件添加的代码
+    public interface OnItemClickListener {
+        void onClick(int position);
+    }
+
+    OnItemClickListener onItemClickListener;
+
+
+    public void setOnItemClickListener(OnItemClickListener onItemClickListener) {
+        this.onItemClickListener = onItemClickListener;
+    }
+
+    public interface OnItemLongClickListener {
+        void onLongClick(int position);
+    }
+
+    OnItemLongClickListener onItemLongClickListener;
+
+    public void setOnItemLongClickListener(OnItemLongClickListener onItemLongClickListener) {
+        this.onItemLongClickListener = onItemLongClickListener;
+    }
+    // 上面是为点击事件添加的代码
+
     @NonNull
     @Override
     public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
@@ -36,8 +59,24 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
     @Override
     public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
         holder.tv.setText(mList.get(position));
+        //自己做item点击
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (onItemClickListener != null)
+                    onItemClickListener.onClick(position);
+            }
+        });
+        holder.itemView.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                if (onItemLongClickListener != null)
+                    onItemLongClickListener.onLongClick(position);
+                //返回false会在长按结束后继续点击
+                return true;
+            }
+        });
     }
-
     @Override
     public int getItemCount() {
         return mList.size();
--- app/src/main/java/com/example/BaiTuanTong_Frontend/PostListDisplayActivity.java
+++ app/src/main/java/com/example/BaiTuanTong_Frontend/PostListDisplayActivity.java
@@ -4,14 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.DefaultItemAnimator;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class PostListDisplayActivity extends AppCompatActivity {
+    public static final String PAGE_EXTRA_MESSAGE = "com.example.BaiTuanTong_Frontend.pageMESSAGE";
+
     private RecyclerView mRecyclerView;
     private MyAdapter mMyAdapter;
     private List<String> mList;
@@ -33,15 +39,37 @@ public class PostListDisplayActivity extends AppCompatActivity {
         mList = getList();
         mMyAdapter = new MyAdapter(this, mList);
         mRecyclerView.setAdapter(mMyAdapter);
+
+        // 下面是为点击事件添加的代码
+        mMyAdapter.setOnItemClickListener(new MyAdapter.OnItemClickListener() {
+            @Override
+            public void onClick(int position) {
+                // Toast.makeText(getBaseContext(), mList.get(position), Toast.LENGTH_SHORT).show();
+                sendMessage(position);
+            }
+        });
+        mMyAdapter.setOnItemLongClickListener(new MyAdapter.OnItemLongClickListener() {
+            @Override
+            public void onLongClick(int position) {
+                //长按删除数据
+                mList.remove(position);
+                mMyAdapter.notifyDataSetChanged();
+            }
+        });
+    }
+
+    public void sendMessage(int position) {
+        Intent intent = new Intent(this, PostPageActivity.class);
+        String message = mList.get(position);
+        intent.putExtra(PAGE_EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 
     private List<String> getList() {
         List<String> list = new ArrayList<>();
-        for (int i = 0; i < 20; i++) {
-            list.add("动态" + i + "");
+        for (int i = 1; i <= 20; i++) {
+            list.add("动态" + i + "\n今天是11月"+ i + "号" + "");
         }
         return list;
     }
-
 }
-
