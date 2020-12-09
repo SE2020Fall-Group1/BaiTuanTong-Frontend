573ba32379a09a8c9bb0d93353e01ad81348c40a
Gufeng <1724949302@qq.com>
2020-12-09 22:22:52 +0800

    Feature:implement get data from backend

--- app/src/main/java/com/example/BaiTuanTong_Frontend/club/ClubHomeActivity.java
+++ app/src/main/java/com/example/BaiTuanTong_Frontend/club/ClubHomeActivity.java
@@ -1,9 +1,13 @@
 package com.example.BaiTuanTong_Frontend.club;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -11,13 +15,34 @@ import androidx.core.app.NavUtils;
 
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.BaiTuanTong_Frontend.R;
+import com.squareup.okhttp.*;
+
+import java.io.IOException;
+
 
 public class ClubHomeActivity extends AppCompatActivity {
 
     private Toolbar mNavigation;
+    private TextView get_result;
+    private OkHttpClient client = new OkHttpClient();
+    private  static final int GET = 1;
+    private Handler getHandler = new Handler(new Handler.Callback() {
+        @Override
+        public boolean handleMessage(@NonNull Message msg) {
+            //super.handleMessage(msg);
+            Log.e("TAG", (String)msg.obj);
+            switch (msg.what){
+                case GET:
+                    get_result.setText((String)msg.obj);
+                    break;
+            }
+            return true;
+        }
+    });
 
     public void initToolBar() {
         mNavigation.setTitle("社团名称");
@@ -43,6 +68,8 @@ public class ClubHomeActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+        get_result = (TextView) findViewById(R.id.get_result);
         //setSupportActionBar(mNavigation);
         //ActionBar actionBar = getSupportActionBar();
 
@@ -56,7 +83,30 @@ public class ClubHomeActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });*/
+        //getDataFromGet();
+    }
 
+    private void getDataFromGet(String url) {
+        //Log.e("TAG", "Start getDataFromGet()");
+        new Thread(){
+            @Override
+            public void run() {
+                super.run();
+                //Log.e("TAG", "new thread run.");
+                try {
+                    //String result = get("http://api.m.mtime.cn/PageSubArea/TrailerList.api");
+                    //String result = get("http://127.0.0.1:5000/");
+                    String result = get(url);
+                    Log.e("TAG", result);
+                    Message msg = Message.obtain();
+                    msg.what = GET;
+                    msg.obj = result;
+                    getHandler.sendMessage(msg);
+                } catch (java.io.IOException IOException) {
+                    Log.e("TAG", "get failed.");
+                }
+            }
+        }.start();
     }
 
     @Override
@@ -74,12 +124,24 @@ public class ClubHomeActivity extends AppCompatActivity {
         switch (item.getItemId())
         {
             case R.id.release_post_menu_item:
-                Toast.makeText(this,"setting is clicked",Toast.LENGTH_SHORT).show();
+                getDataFromGet("http://api.m.mtime.cn/PageSubArea/TrailerList.api");
+                //Toast.makeText(this,"setting is clicked",Toast.LENGTH_SHORT).show();
                 break;
             case R.id.club_admin_manage_menu_item:
-                Toast.makeText(this,"favorite is clicked",Toast.LENGTH_SHORT).show();
+                getDataFromGet("http://10.0.2.2:5000/hello");
+                //Toast.makeText(this,"favorite is clicked",Toast.LENGTH_SHORT).show();
                 break;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+
+
+    private String get(String url) throws IOException {
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+        Response response = client.newCall(request).execute();
+        return response.body().string();
     }
 }
