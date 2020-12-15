aecd4e6ca0fadcfd469804000fb525a7fb208ae5
zshCuanNi <zsh2000911@outlook.com>
2020-12-16 01:09:50 +0800

    Fix: fix errors occured when import com.google.gson.Gson
    Delete gson in dependencies and add gson JAR files as external library.

--- app/src/main/java/com/example/BaiTuanTong_Frontend/ui/login/LoginActivity.java
+++ app/src/main/java/com/example/BaiTuanTong_Frontend/ui/login/LoginActivity.java
@@ -1,52 +1,36 @@
 package com.example.BaiTuanTong_Frontend.ui.login;
 
-import android.app.Activity;
-
-import androidx.annotation.NonNull;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProvider;
-
 import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.annotation.Nullable;
-import androidx.annotation.StringRes;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Handler;
 import android.os.Message;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
-import android.view.KeyEvent;
 import android.view.View;
-import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
-import android.widget.TextView;
 import android.widget.Toast;
 
-import com.example.BaiTuanTong_Frontend.PostContentActivity;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.annotation.StringRes;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
 import com.example.BaiTuanTong_Frontend.R;
-import com.example.BaiTuanTong_Frontend.data.model.LoggedInUser;
-import com.example.BaiTuanTong_Frontend.ui.login.LoginViewModel;
-import com.example.BaiTuanTong_Frontend.ui.login.LoginViewModelFactory;
-import com.example.BaiTuanTong_Frontend.ui.register.RegistActivity;
 import com.example.BaiTuanTong_Frontend.home.HomePageActivity;
+import com.example.BaiTuanTong_Frontend.ui.register.RegistActivity;
 import com.google.gson.Gson;
 
-import org.jetbrains.annotations.NotNull;
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.IOException;
 import java.util.HashMap;
 
-import okhttp3.Call;
-import okhttp3.Callback;
-import okhttp3.FormBody;
 import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
