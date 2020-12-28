30b6e342057802a08b1939a7a5738b8628132453
zhp-99 <1800012831@pku.edu.cn>
2020-12-28 23:39:31 +0800

    Feature:可以显示没有图片的动态

--- app/src/main/java/com/example/BaiTuanTong_Frontend/PostContentActivity.java
+++ app/src/main/java/com/example/BaiTuanTong_Frontend/PostContentActivity.java
@@ -146,6 +146,8 @@ public class PostContentActivity extends AppCompatActivity {
                             Log.e("url","any");
                             imageUrlList.add(imageUrl);
                         }
+                        if(imageUrlJSONArray.length()!=0)
+                            contentImageView.setImageDrawable(getResources().getDrawable(R.drawable.post_content_pic));
                         //申请图片
                         Log.e("img","get img");
                         getImgFromUrl();
